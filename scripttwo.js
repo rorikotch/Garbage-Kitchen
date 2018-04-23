56f8ef753e794cfc25a758ad86917e19b424ff60
Rori Kotch <r.kotch1@umiami.edu>
2018-04-23 14:43:18 -0400

    Added more code

--- scripttwo.js
+++ scripttwo.js
@@ -1,22 +1,4 @@
-/*this script is used for interavtive visualizations that are made using highcharts.js*/
-
-/* this is the documentation for a basic pie chart using highcharts*/
-// Make monochrome colors
-var pieColors = (function () {
-    var colors = [],
-        base = Highcharts.getOptions().colors[0],
-        i;
-
-    for (i = 0; i < 10; i += 1) {
-        // Start out with a darkened base color (negative brighten), and end
-        // up with a much brighter color
-        colors.push(Highcharts.Color(base).brighten((i - 3) / 7).get());
-    }
-    return colors;
-}());
-
-// Build the chart
-Highcharts.chart('container', {
+Highcharts.chart('graphone', {
     chart: {
         plotBackgroundColor: null,
         plotBorderWidth: null,
@@ -24,7 +6,7 @@ Highcharts.chart('container', {
         type: 'pie'
     },
     title: {
-        text: 'Browser market shares at a specific website, 2014'
+        text: 'Browser market shares in January, 2018'
     },
     tooltip: {
         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -33,36 +15,47 @@ Highcharts.chart('container', {
         pie: {
             allowPointSelect: true,
             cursor: 'pointer',
-            colors: pieColors,
             dataLabels: {
                 enabled: true,
-                format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
-                distance: -50,
-                filter: {
-                    property: 'percentage',
-                    operator: '>',
-                    value: 4
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
                 }
             }
         }
     },
     series: [{
-        name: 'Share',
-        data: [
-            { name: 'Chrome', y: 61.41 },
-            { name: 'Internet Explorer', y: 11.84 },
-            { name: 'Firefox', y: 10.85 },
-            { name: 'Edge', y: 4.67 },
-            { name: 'Safari', y: 4.18 },
-            { name: 'Other', y: 7.05 }
-        ]
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 61.41,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Internet Explorer',
+            y: 11.84
+        }, {
+            name: 'Firefox',
+            y: 10.85
+        }, {
+            name: 'Edge',
+            y: 4.67
+        }, {
+            name: 'Safari',
+            y: 4.18
+        }, {
+            name: 'Sogou Explorer',
+            y: 1.64
+        }, {
+            name: 'Opera',
+            y: 1.6
+        }, {
+            name: 'QQ',
+            y: 1.2
+        }, {
+            name: 'Other',
+            y: 2.61
+        }]
     }]
 });
-
-/* this is the end of the documentation on how to make a basic pie chart with highcharts.js*/
-
-/* these are the scripts you need to link in order to use highcharts
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/export-data.js"></script>
-/* 
